71fbe5630a9a11c596c44971c1870ef7707fb464
heng.wang <heng.wang@sao365.cn>
2017-08-15 15:36:26 +0800

    fix start bug

--- src/main/java/so/sao/wechat/mini/pay/config/WxPayAutoConfiguration.java
+++
@@ -1,9 +0,0 @@
-package so.sao.wechat.mini.pay.config;
-
-import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
-
-
-@ConditionalOnProperty(prefix = "wechat.pay", name = {"app-id", "mch-id", "mch-key"})
-public class WxPayAutoConfiguration {
-
-}
